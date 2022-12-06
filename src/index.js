94b0a33137314eed6f2b446834911c116be2af65
Ali Yanni <aliryanni@gmail.com>
2022-12-06 17:27:20 -0500

    grid practice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./index.css";
 
-function Greeting() {
-  return <h4> This is Ali and this is my first componet.</h4>;
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81S+-1xB4WL._AC_UL210_SR195,210_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>From Crook to Cook</h1>;
+
+const Author = () => <h4>Snoop Dogg</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
